379bca80b78378e991d82ceae9a9dcf3720d9966
Nakul Koshal <nakulkoshal4@gmail.com>
2024-10-24 01:39:49 +0530

    Update tb.v

--- tb.v
+++ tb.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.10.2024 09:47:07
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module tb();
