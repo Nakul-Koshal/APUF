1fffbc386e1c4ccf41e34c268b47db266f2b4b8c
Nakul Koshal <nakulkoshal4@gmail.com>
2024-10-24 01:39:10 +0530

    Update PUF_32.v

--- PUF_32.v
+++ PUF_32.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.10.2024 08:02:14
-// Design Name: 
-// Module Name: PUF_32
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module PUF_32
