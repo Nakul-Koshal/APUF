5d786e58da3b9a9edcda1afde5e2ec36ecc49280
Nakul Koshal <nakulkoshal4@gmail.com>
2024-10-24 06:39:38 +0530

    Update PUF_4.v

--- PUF_4.v
+++ PUF_4.v
@@ -10,7 +10,6 @@ output mkg1,mkg2,mkg3,mkg4 //Outputs from MKG gate
 );
 
 wire [4:1]dff; //Outputs from D flip-flops in Arbiters 
-wire ww3;
 
 //4 arbiter as we have 4 input MKG gate 
 arbiter arb1(clk,rst,c_bits,dff[1]);
@@ -18,7 +17,6 @@ arbiter arb2(clk,rst,c_bits,dff[2]);
 arbiter arb3(clk,rst,c_bits,dff[3]);
 arbiter arb4(clk,rst,c_bits,dff[4]);
 
-MKG_gate mkg(dff[1],dff[2],dff[3],dff[4],mkg1,mkg2,ww3,mkg4);
-assign mkg3 = mkg1 ^ ww3;
+MKG_gate mkg(dff[1],dff[2],dff[3],dff[4],mkg1,mkg2,mkg3,mkg4);
 
 endmodule
